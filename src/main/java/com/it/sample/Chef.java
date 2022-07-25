5b8c952de278dd117c7982fbe8897ebaa94b4ce9
KITT-51\isec <w2nd33@naver.com>
2022-07-25 20:21:32 +0900

    First Commit

---
+++ src/main/java/com/it/sample/Chef.java
@@ -0,0 +1,11 @@
+package com.it.sample;
+
+import org.springframework.stereotype.Component;
+
+import lombok.Data;
+
+@Component
+@Data
+public class Chef {
+
+}
---
+++ src/main/java/com/it/sample/Restaurant.java
@@ -0,0 +1,20 @@
+package com.it.sample;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Component;
+
+import lombok.RequiredArgsConstructor;
+import lombok.ToString;
+
+//Setter 주입
+//생성자 주입
+//필드 주입
+@Component
+@ToString
+@RequiredArgsConstructor
+public class Restaurant {
+	
+	@Autowired
+	private final Chef chef;
+
+}
